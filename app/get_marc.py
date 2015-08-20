7a59e9bec9191f57013eb2e08d669caebb831f79
Lawrence Siebert <lawrencesiebert@gmail.com>
2015-08-20 15:20:53 -0700

    get_marc added todos, removal of existing file

--- app/get_marc.py
+++ app/get_marc.py
@@ -5,17 +5,21 @@ docstring get_marc.py
 """
 import urllib2
 from bz2 import BZ2Decompressor
+from os import remove
 
 
+# todo add code to compare with existing file
 def download_success(in_file, out_file_name):
     return int(in_file.getcode()) == 200
 
 
+# todo store immediately in database
 def get_bz2_from_file(in_file, out_file_name, file_is_good):
     if not file_is_good(in_file, out_file_name):
         return False
     bz2 = BZ2Decompressor()
     CHUNK = 16384
+    remove(out_file_name)
     with open(out_file_name, 'wb') as fp:
         while True:
             chunk = in_file.read(CHUNK)
