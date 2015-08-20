0ba86670a7eede6038d63ad768d64e3471d3b063
Lawrence Siebert <lawrencesiebert@gmail.com>
2015-08-20 15:30:02 -0700

    get_marc added file existence test before file removal

--- app/get_marc.py
+++ app/get_marc.py
@@ -5,7 +5,7 @@ docstring get_marc.py
 """
 import urllib2
 from bz2 import BZ2Decompressor
-from os import remove
+from os import remove, path
 
 
 # todo add code to compare with existing file
@@ -13,13 +13,14 @@ def download_success(in_file, out_file_name):
     return int(in_file.getcode()) == 200
 
 
-# todo store immediately in database
+# todo add function to handle marc file and store immediately in database
 def get_bz2_from_file(in_file, out_file_name, file_is_good):
     if not file_is_good(in_file, out_file_name):
         return False
     bz2 = BZ2Decompressor()
     CHUNK = 16384
-    remove(out_file_name)
+    if path.isfile(out_file_name):
+        remove(out_file_name)
     with open(out_file_name, 'wb') as fp:
         while True:
             chunk = in_file.read(CHUNK)
